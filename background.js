abd9654bc066b4c1fca754639bcee1340ed4a10d
Tomas Kliner <tomasklinerr@gmail.com>
2022-08-05 19:42:58 +0200

    working keyword without local storage

--- background.js
+++ background.js
@@ -1,29 +1,11 @@
-/*chrome.runtime.onMessage.addListener((message, callback) => {
-    if (message.data === "loadedPage") {
-        alert('page loaded');
-        const tabId = getForegroundTabId();
-
-        let pagesTo = [];
-        let pagesFrom = [];
-        if (localStorage.getItem('pagesTo')) {
-            pagesTo = JSON.parse(localStorage.getItem('pagesTo'));
-            pagesFrom = JSON.parse(localStorage.getItem('pagesFrom'));
-        }
-        let url = message.data;
-        for (let i = 0; i < pagesFrom.length; ++i) {
-            if (pagesFrom[i] == url) {
-                chrome.tabs.update(tabId, { url: pagesTo[i] });
-                break;
-            }
-        } 1
-        alert("working");
-    }
-});*/
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    chrome.tabs.update(tabId, 'www.google.com');
+    localStorage.getItem['keywordsFrom']
+    let surl = changeInfo['url'];
+    if(surl.includes('valkyrae')){
+        chrome.tabs.update(tabId, {url: "https://www.google.com"});
+    }
  }); 
-
 
 
 
