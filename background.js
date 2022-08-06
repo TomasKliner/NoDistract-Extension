44d56e0502ea0662a519c71f11d39ae0f6f2a774
Tomas Kliner <tomasklinerr@gmail.com>
2022-08-06 16:23:27 +0200

    added url functionality, reformatted layout workable version

--- background.js
+++ background.js
@@ -3,17 +3,29 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
     chrome.storage.local.get('keywords', function (result) {
         let arr = result['keywords'] ? result['keywords'] : [];
-        console.log(arr);
-
         let surl = changeInfo['url'];
-        console.log(arr);
 
         for (let i = 0; i < arr.length; ++i) {
             if (surl.includes(arr[i])) {
+                console.log(surl);
                 chrome.tabs.update(tabId, { url: "https://www.google.com" });
             }
         }
     });
+
+
+    chrome.storage.local.get('urls', function (result) {
+        let arr = result['urls'] ? result['urls'] : [];
+        let surl = changeInfo['url'];
+
+        for (let i = 0; i < arr.length; ++i) {
+            if (surl == arr[i]) {
+                console.log(surl);
+                chrome.tabs.update(tabId, { url: "https://www.google.com" });
+            }
+        }
+    });
+
 
 });
 
