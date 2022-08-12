1238f76579dc80762243c70e260e4cd2a525c25c
Tomas Kliner <tomasklinerr@gmail.com>
2022-08-12 15:25:56 +0200

    Working backgroudn script

--- scripts/background.js
+++ scripts/background.js
@@ -1,83 +1,117 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     old();
-    chrome.storage.local.get('permanent', function (result) {
-        let arr = result['permanent']['keys'] ? result['permanent']['keys'] : [];
-        let surl = changeInfo['url'];
-
-        for (let i = 0; i < arr.length; ++i) {
-            if (surl.includes(arr[i])) {
-                chrome.tabs.update(tabId, { url: "https://www.google.com" });
-            }
-        }
-    });
-
-    chrome.storage.local.get('permanent', function (result) {
-        let arr = result['permanent']['urls'] ? result['permanent']['urls'] : [];
-        let surl = changeInfo['url'];
-
-        for (let i = 0; i < arr.length; ++i) {
-            if (surl == arr[i]) {
-                console.log(surl);
-                chrome.tabs.update(tabId, { url: "https://www.google.com" });
-            }
-        }
-    });
-
-});
+    permanent();
+    global();
 
 
 
+    function global() {
+        chrome.storage.local.get('global', function (result) {
 
+            let now = getTime();
 
+            if ((Date.parse('01/01/2011 ' + result['global']['timeFrom']) < Date.parse('01/01/2011 ' + now)) && (Date.parse('01/01/2011 ' + result['global']['timeTo']) > Date.parse('01/01/2011 ' + now))) {
 
-
-function old() {
-    chrome.storage.local.get('time', function (result) {
-
-        let timeArr = result['time'] ? result['time'] : [];
-        let isInInterval = false;
-
-        const timeNow = new Date('2020-01-01 ' + getTime());
-
-
-        for (let i = 0; i < timeArr.length; i += 2) {
-            let timeF = new Date('2020-01-01 ' + timeArr[i]);
-            let timeT = new Date('2020-01-01 ' + timeArr[i + 1]);
-            if (timeNow >= timeF && timeNow <= timeT) {
-                isInInterval = true;
-                break;
-            }
-        }
-        if (1) {
-            chrome.storage.local.get('keywords', function (result) {
-                let arr = result['keywords'] ? result['keywords'] : [];
+                let arr = result['global']['keys'] ? result['global']['keys'] : [];
                 let surl = changeInfo['url'];
+
 
                 for (let i = 0; i < arr.length; ++i) {
                     if (surl.includes(arr[i])) {
                         chrome.tabs.update(tabId, { url: "https://www.google.com" });
                     }
                 }
-            });
 
 
-            chrome.storage.local.get('urls', function (result) {
-                let arr = result['urls'] ? result['urls'] : [];
-                let surl = changeInfo['url'];
+                let arr2 = result['global']['urls'] ? result['global']['urls'] : [];
 
-                for (let i = 0; i < arr.length; ++i) {
-                    if (surl == arr[i]) {
-                        console.log(surl);
+                for (let i = 0; i < arr2.length; ++i) {
+                    if (surl == arr2[i]) {
+
                         chrome.tabs.update(tabId, { url: "https://www.google.com" });
                     }
                 }
-            });
-        }
-    });
-}
-function getTime() {
-    const date = new Date();
-    return date.getHours() + ":" + date.getMinutes();
-}
 
-let tempArr = ['youtube.com/shorts', 'youtube.com/feed/explore']
+            }
+        }
+        );
+    }
+
+
+
+    function permanent() {
+        chrome.storage.local.get('permanent', function (result) {
+            let arr = result['permanent']['keys'] ? result['permanent']['keys'] : [];
+            let surl = changeInfo['url'];
+
+            for (let i = 0; i < arr.length; ++i) {
+                if (surl.includes(arr[i])) {
+                    chrome.tabs.update(tabId, { url: "https://www.google.com" });
+                }
+            }
+
+            let arr2 = result['permanent']['urls'] ? result['permanent']['urls'] : [];
+
+            for (let i = 0; i < arr2.length; ++i) {
+                if (surl == arr2[i]) {
+                    console.log(surl);
+                    chrome.tabs.update(tabId, { url: "https://www.google.com" });
+                }
+            }
+        });
+    }
+
+
+
+
+
+    function old() {
+        chrome.storage.local.get('time', function (result) {
+
+            let timeArr = result['time'] ? result['time'] : [];
+            let isInInterval = false;
+
+            const timeNow = new Date('2020-01-01 ' + getTime());
+
+
+            for (let i = 0; i < timeArr.length; i += 2) {
+                let timeF = new Date('2020-01-01 ' + timeArr[i]);
+                let timeT = new Date('2020-01-01 ' + timeArr[i + 1]);
+                if (timeNow >= timeF && timeNow <= timeT) {
+                    isInInterval = true;
+                    break;
+                }
+            }
+            if (1) {
+                chrome.storage.local.get('keywords', function (result) {
+                    let arr = result['keywords'] ? result['keywords'] : [];
+                    let surl = changeInfo['url'];
+
+                    for (let i = 0; i < arr.length; ++i) {
+                        if (surl.includes(arr[i])) {
+                            chrome.tabs.update(tabId, { url: "https://www.google.com" });
+                        }
+                    }
+                });
+
+
+                chrome.storage.local.get('urls', function (result) {
+                    let arr = result['urls'] ? result['urls'] : [];
+                    let surl = changeInfo['url'];
+
+                    for (let i = 0; i < arr.length; ++i) {
+                        if (surl == arr[i]) {
+                            console.log(surl);
+                            chrome.tabs.update(tabId, { url: "https://www.google.com" });
+                        }
+                    }
+                });
+            }
+        });
+    }
+    function getTime() {
+        const date = new Date();
+        return date.getHours() + ":" + date.getMinutes();
+    }
+});
+let tempArr = ['youtube.com/shorts', 'youtube.com/feed/explore'];
