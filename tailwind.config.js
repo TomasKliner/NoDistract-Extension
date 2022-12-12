485e619745dea8da8b61766ccd5a9323b3dbe443
Tomáš Kliner <klinetom@fit.cvut.cz>
2022-12-12 21:46:12 +0100

    Clear directory

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-}
-// startup script -> lepší využít npm script
-//npx tailwindcss -i ./styles/input.css -o ./styles/output.css --watch
