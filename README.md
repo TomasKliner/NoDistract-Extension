050a9e143f5c002b3b1ad1e0a971bf5a20e3babd
Tomáš Kliner <klinetom@fit.cvut.cz>
2022-12-13 19:54:52 +0100

    first working settings page

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
+# NoDistract-Extension :-)
+
+___
+## Notes
+- Use chrome.sdtorage.local or chrome.storage.sync
+  - https://stackoverflow.com/questions/24279495/window-localstorage-vs-chrome-storage-local
+
+- Context Script cannot acces local storage, but chrome storage yes.
+
+
+### Scripts
+- Use chrome.ruintime messaging to pass data to background.js
+
+- content script is isolated and have to pass data to background script
+
+- BG script handless events and messages.
+
+
+
 This is a [Plasmo extension](https://docs.plasmo.com/) project bootstrapped with [`plasmo init`](https://www.npmjs.com/package/plasmo).
 
 ## Getting Started
--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,5 +1,56 @@
+import React, { useEffect } from "react"
+
+import { Storage } from "@plasmohq/storage"
+
+import "../../style.css"
+
+import Sidebar from "../components/sidebar"
+
+const storage = new Storage()
 
 export default function options() {
-  return (<h1>working</h1>)
-}
+  const [redirectUrl, setRedirectUrl] = React.useState(undefined)
 
+  React.useEffect(() => {
+    let loadData = async () => {
+      let value = await storage.get("redirectUrl")
+      console.log(value);
+      setRedirectUrl(value)
+    }
+    loadData()
+  }, [])
+
+  React.useEffect(() => {
+    let asyncWork = async () => {
+        console.log("saved");
+      await storage.set("redirectUrl", redirectUrl)
+    }
+    redirectUrl !== undefined && asyncWork()
+  }, [redirectUrl])
+
+  function handleUrl(event){
+    setRedirectUrl(event.target.value)
+  }
+  return (
+    <div className="flex justify-between h-screen w-screen">
+      <Sidebar />
+
+      <div className="text-black w-full text-center overflow-y-scroll min-h-screen">
+        <h1 className="text-2xl">Main settings</h1>
+        <label className="flex justify-center">
+          <h3>page you get reddirected to:</h3>
+          <input
+            type="text"
+            id="redirectUrl"
+            className="input input-bordered p-2 mx-2 border-2 border-black"
+            placeholder="google.com"
+            value={redirectUrl}
+            onChange={handleUrl}></input>
+        </label>
+        <button id="save" className="block m-auto btn btn-accent">
+          SAVE // does nothing
+        </button>
+      </div>
+    </div>
+  )
+}
