4ba831f7f6dd70efc00f222eddf7c046d64b94f2
Tomáš Kliner <klinetom@fit.cvut.cz>
2022-12-13 19:55:10 +0100

    working plasmo storage api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
-import { Main } from "~components/main"
+export default function IndexPage() {
+  return (<html>
 
-function IndexPage() {
-  return <Main name="App" />
+    <head>
+        <link rel="stylesheet" href="../styles/output.css">
+    </head>
+    
+    <body className="bg-neutral-800 w-80 h-96 text-white">
+        <div>
+            <h2 className="text-center">Time Spent (Today)</h2>
+            <p className="text-center">time</p>
+            <h2 className="text-center">Time left</h2>
+            <p className="text-center">time</p>
+        </div>
+        <a href="./settings.html">
+            <div className="w-16 h-16 rounded-full bg-white text-black flex items-center text-center font-bold shadow hover:bg-neutral-200">Settings</div>
+        </a>
+    
+    </body>
+    
+    <script src="../scripts/index.js"></script>
+    <script src="../scripts/time.js"></script>
+    </html>
+    )
 }
 
-export default IndexPage
