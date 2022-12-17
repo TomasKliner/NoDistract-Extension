d973ef553f278a40750775f68362196c4c8a3673
Tomas Kliner <tomasklinerr@gmail.com>
2022-12-17 10:47:39 +0100

    limits component update

--- src/tabs/limits.tsx
+++ src/tabs/limits.tsx
@@ -8,6 +8,28 @@ import Sidebar from "../components/sidebar"
 
 const storage = new Storage()
 
+export function Website(props) {
+  const [duration, setDuration] = React.useState(props.number);
+  const [website, setWebsite] = React.useState(props.website);
+  return (
+    <div className="flex justify-center w-full mb-1">
+      <input
+        type="text"
+        value={website}
+        onChange={(e) => setWebsite(e.target.value)}
+        className="border-2 border-blue-500 rounded-l w-1/2 shadow p-1"></input>
+      <input
+        type="number"
+        value={duration}
+        onChange={(e) => setDuration(e.target.value)}
+        className="border-2 border-cyan-500 w-16 shadow p-1 text-center"></input>
+      <span className="bg-red-500 rounded-r-full border-2 border-red-700 h-full text-white py-1 px-2 font-bold  shadow hover:cursor-pointer">
+        x
+      </span>
+    </div>
+  )
+}
+
 export default function Limits() {
   return (
     <div className="flex justify-between h-screen w-screen">
@@ -22,16 +44,10 @@ export default function Limits() {
             <h2 className="w-48 text-cyan-500">Minutes</h2>
           </div>
           <div className="">
-            <div className="flex justify-center w-full mb-1">
-              <input
-                type="text"
-                className="border-2 border-blue-500 rounded-l w-1/2 shadow p-1"></input>
-              <input
-                type="number"
-                className="border-2 border-cyan-500 w-16 shadow p-1"></input>
-              <span className="bg-red-500 rounded-r-full border-2 border-red-700 h-full text-white py-1 px-2 font-bold  shadow hover:cursor-pointer">x</span>
-            </div>
-            <button id="save" className="button m-auto font-medium px-4 rounded-full shadow-md shadow-cyan-500">
+            <Website website="nwm" number="5" />
+            <button
+              id="save"
+              className="button m-auto font-medium px-4 rounded-full shadow-md shadow-cyan-500">
               Add
             </button>
           </div>
